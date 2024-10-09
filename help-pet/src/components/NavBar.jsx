70b75ec25ad9df982b6237f9e8c0f2530ee7e1fd
David-Evangelist <dev.davidwilliam@gmail.com>
2024-10-09 01:34:12 -0300

    new commit

--- help-pet/src/components/NavBar.jsx
+++ help-pet/src/components/NavBar.jsx
@@ -1,30 +1,42 @@
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+
 import logo from "../assets/logo.png";
 import styles from "../style/nav_bar.module.css";
+
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className={styles.navBar_container}>
-      <div className={styles.containerImg}>
-        <img src={logo} alt="Logo HelpPet" />
-        <p>HelpPet</p>
-      </div>
+    <div className={styles.container}>
+      
+      <nav className={styles.navBar_container}>
+        
+        <div className={styles.containerImg}>
+          <img src={logo} alt="Logo HelpPet" />
+          <p>Help Pet</p>
+        </div>
 
-      <ul className={styles.containerList}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">Sobre nós</Link>
-        </li>
-        <li>
-          <Link to="/services">Serviços</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className={styles.containerList}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre nós</Link>
+          </li>
+          <li>
+            <Link to="/services">Serviços</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+
+      </nav>
+
+      <div className={styles.line}></div>
+
+    </div>
   );
 }
 
--- help-pet/src/pages/Login.jsx
+++ help-pet/src/pages/Login.jsx
@@ -1,11 +1,29 @@
+import cat1 from "../assets/cat1.png";
+import dog1 from "../assets/dog1.png";
+import styles from "../style/login.module.css";
+
 import NavBar from "../components/NavBar";
 
 function Login() {
   return (
-    <div>
-      <NavBar />
-      <p>LOGIN</p>
-    </div>
+    <body className={styles.bodyLogin}>
+        <NavBar/>
+
+        <div className={styles.login}>
+
+            <img src={dog1} alt="Dog" className={styles.imgAnimals} />
+
+            <div className={styles.cardFormLogin}>
+                <h1>LOGIN</h1>
+                <p>OI</p>
+            </div>
+
+            <img src={cat1} alt="Cat" className={styles.imgAnimals} />
+
+        </div>
+        
+    </body>
+
   );
 }
 
